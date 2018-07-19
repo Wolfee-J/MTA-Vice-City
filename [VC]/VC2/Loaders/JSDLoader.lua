deba7f5ae7f4b390b00cc07713dc152e821e4337
CodyJ <codylewiz@gmail.com>
2018-07-19 10:39:22 -0400

    Updated Indentation

--- [VC]/VC2/Loaders/JSDLoader.lua
+++ [VC]/VC2/Loaders/JSDLoader.lua
@@ -6,21 +6,21 @@ local Proccessed = split(Data,10)
 fileClose (File)
 
 function toBoolean(input)
-if input == 'true' then
-return true
-else
-return false
-end
+	if input == 'true' then
+		return true
+	else
+		return false
+	end
 end
 
 local resource =  getResourceName(getThisResource())
 
 
 function Load()
-for iA,vA in pairs(Proccessed) do
-local SplitA = split(vA,",")
-exports.ObjS:JCreateObjectDefinition(SplitA[1],':'..resource..'/Content/models/'..SplitA[2]..'.dff',':'..resource..'/Content/textures/'..SplitA[3]..'.txd',':'..resource..'/Content/coll/'..SplitA[4]..'.col',SplitA[5],toBoolean(SplitA[6]),toBoolean(SplitA[7]),toBoolean(SplitA[8]),SplitA[9] or nil,SplitA[10] or nil)
-end
+	for iA,vA in pairs(Proccessed) do
+		local SplitA = split(vA,",")
+		exports.ObjS:JCreateObjectDefinition(SplitA[1],':'..resource..'/Content/models/'..SplitA[2]..'.dff',':'..resource..'/Content/textures/'..SplitA[3]..'.txd',':'..resource..'/Content/coll/'..SplitA[4]..'.col',SplitA[5],toBoolean(SplitA[6]),toBoolean(SplitA[7]),toBoolean(SplitA[8]),SplitA[9] or nil,SplitA[10] or nil)
+	end
 end
 
 setTimer(Load,1000,1)
@@ -32,7 +32,7 @@ addEventHandler ( "onClientResourceStop", resourceRoot,
     function (  )
 		for iA,vA in pairs(Proccessed) do
 			local SplitA = split(vA,",")
-				exports.ObjS:unloadModel(SplitA[1])
+			exports.ObjS:unloadModel(SplitA[1])
 		end
    end 
 )
--- [VC]/VC2/Loaders/JSPLoader.lua
+++ [VC]/VC2/Loaders/JSPLoader.lua
@@ -1,7 +1,7 @@
 debug.sethook(nil)
 
-local File =  fileOpen('gta3.JSP')   
-local Data =  fileRead(File, fileGetSize(File))
+local File = fileOpen('gta3.JSP')
+local Data = fileRead(File, fileGetSize(File))
 local Proccessed = split(Data,10)
 fileClose (File)
 
@@ -13,14 +13,14 @@ for iA,vA in pairs(Proccessed) do
 	if iA == 1 then
 		local x,y,z = split(vA,",")[1],split(vA,",")[2],split(vA,",")[3]
 		XA,YA,ZA = tonumber(x),tonumber(y),tonumber(z)
-			else
-				local SplitA = split(vA,",")
-			if not (SplitA[1] == '!') then -- IF #1 == ! THEN IGNORE
+	else
+		local SplitA = split(vA,",")
+		if not (SplitA[1] == '!') then -- IF #1 == ! THEN IGNORE
 
-				local object = exports.ObjS:JcreateObject(SplitA[1],tonumber(SplitA[4])+XA,tonumber(SplitA[5])+YA,tonumber(SplitA[6])+ZA,tonumber(SplitA[7]),tonumber(SplitA[8]),tonumber(SplitA[9]))
+			local object = exports.ObjS:JcreateObject(SplitA[1],tonumber(SplitA[4])+XA,tonumber(SplitA[5])+YA,tonumber(SplitA[6])+ZA,tonumber(SplitA[7]),tonumber(SplitA[8]),tonumber(SplitA[9]))
 			if object then
-					setElementInterior(object,tonumber(SplitA[2]))
-					setElementDimension(object,tonumber(SplitA[3]))
+				setElementInterior(object,tonumber(SplitA[2]))
+				setElementDimension(object,tonumber(SplitA[3]))
 				table.insert(ObjectList,object)
 			end
 		end
@@ -28,13 +28,13 @@ for iA,vA in pairs(Proccessed) do
 end
 
 
-addEventHandler ( "onResourceStop", resourceRoot, 
-    function (  )
-		for iA,vA in pairs(ObjectList) do
-			if isElement(vA) then
-				destroyElement(vA)
-			end
+addEventHandler ( "onResourceStop", resourceRoot,
+function (	)
+	for iA,vA in pairs(ObjectList) do
+		if isElement(vA) then
+			destroyElement(vA)
 		end
-   end 
+	end
+end
 )
 
