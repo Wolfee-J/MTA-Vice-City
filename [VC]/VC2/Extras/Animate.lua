9b381991a4f3371912d688d9415bd437cc06dda0
Wolfee <bluejaycjl@gmail.com>
2020-07-01 17:53:53 -0400

    Major Update
    Fixed some collisions
    Fixed some models
    Updated to MSD / MSP
    Remove client side CSD
    Etc etc

--- [VC]/VC2/Extras/Animate.lua
+++ [VC]/VC2/Extras/Animate.lua
@@ -8,12 +8,11 @@ An['ap_radar1_01'] = 2
 
 Animation = {}
 for i,v in pairs(getElementsByType('object')) do
-	if An[getElementID(v)] then
-		table.insert(Animation,{v,An[getElementID(v)]})
-			local xr,yr,zr = getElementRotation(v)
-			setElementRotation(v,xr,yr,zr+math.random(1,10))
-			local xra,yra,zra = getElementRotation(v)
-
+	if An[getElementData(v,'id')] then
+		table.insert(Animation,{v,An[getElementData(v,'id')]})
+		local xr,yr,zr = getElementRotation(v)
+		setElementRotation(v,xr,yr,zr+math.random(1,10))
+		local xra,yra,zra = getElementRotation(v)
 	end
 end
 
@@ -25,10 +24,10 @@ function updateCamera ()
 		setElementRotation(v[1],xr,yr,zr+v[2])
 		
 		if getLowLODElement(v[1]) then
-				setElementRotation(getLowLODElement(v[1]),xr,yr,zr+v[2] )
-		end
-		
+			setElementRotation(getLowLODElement(v[1]),xr,yr,zr+v[2] )
 		end
 	end
+end
+
 addEventHandler ( "onClientRender", root, updateCamera )
 
